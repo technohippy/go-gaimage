347a17877d8198f105da814488661c6aa3259885
ANDO Yasushi <yasushi.ando@kabuku.co.jp>
2021-02-27 00:39:41 +0900

    -

--- gaimage/ga.go
+++ gaimage/ga.go
@@ -13,22 +13,39 @@ import (
 	"time"
 )
 
+const UseAlpha = true
+const UseGeneMutate = true // true:mutate false:replace
+
+//const ImageName = "fuji"
+const ImageName = "germany"
 const ImageSize = 100
+
 const GenrationCount = 50000
-const PopulationCount = 100
-const EliteCount = 50
+const PopulationCount = 40
+const EliteCount = PopulationCount / 2
 const TournamentCount = 2
-const GeneCount = 200
+
+const GeneCount = 100
+const MutateRatio = 0.5
+
+//const GeneCount = 20
+//const MutateRatio = 0.5
+
+const MutateProbability = 0.1
 const ShapeSizeMin = 4
-const ShapeSizeMax = 20
+const ShapeSizeMax = 30
 const LocusCount = 10
-const MutateRatio = 0.3
 
 const LogStride = 100
 
 func Run() {
-	targetImage, _ := os.Open(fmt.Sprintf("./soba%v.png", ImageSize))
+	rand.Seed(time.Now().UnixNano())
+
+	targetImage, err := os.Open(fmt.Sprintf("./images/%v%v.png", ImageName, ImageSize))
 	defer targetImage.Close()
+	if err != nil {
+		log.Fatal("io error ", err)
+	}
 	target, err := png.Decode(targetImage)
 	if err != nil {
 		log.Fatal("decode error ", err)
@@ -41,13 +58,20 @@ func Run() {
 		p.PrintAverageFitness()
 
 		if i%LogStride == 0 {
+			c := p.Survivor()
+			c.CheckGenes()
+			img := c.Decode()
+
 			func() {
-				c := p.Survivor()
-				c.CheckGenes()
-				img := c.Decode()
-				f, _ := os.Create(fmt.Sprintf("./results/gen-%05d-%05d.png", p.Generation, int(c.fitness/1e10)))
+				f, _ := os.Create("./results/current.png")
 				defer f.Close()
 				png.Encode(f, img)
+
+				if i%(LogStride*10) == 0 {
+					f, _ = os.Create(fmt.Sprintf("./results/gen-%05d-%05d.png", p.Generation, int(c.Fitness/1e10)))
+					defer f.Close()
+					png.Encode(f, img)
+				}
 			}()
 		}
 	}
@@ -90,14 +114,23 @@ func NewPopulation(target image.Image, num int) *Population {
 func (p *Population) Next() {
 	p.sortIndividualsByFitness()
 
+	next := make([]*Chromosome, len(p.Individuals))
+
 	elites := p.Individuals[:EliteCount]
-	next := append([]*Chromosome{}, elites...)
+	for i, elite := range elites {
+		next[i] = elite.Clone()
+	}
+
 	for i := 0; i < PopulationCount-EliteCount; i++ {
-		i1 := p.tournament(TournamentCount)
-		i2 := p.tournament(TournamentCount)
+		i1 := p.roulette()
+		i2 := p.roulette()
+		//i1 := p.tournament(TournamentCount)
+		//i2 := p.tournament(TournamentCount)
 		i3 := i1.Intersect(i2)
-		i3.Mutate(int(GeneCount * MutateRatio))
-		next = append(next, i3)
+		if rand.Float64() < MutateProbability {
+			i3.Mutate(int(GeneCount * MutateRatio))
+		}
+		next[EliteCount+i] = i3
 	}
 	p.Individuals = next
 	p.Generation += 1
@@ -110,17 +143,34 @@ func (p *Population) Survivor() *Chromosome {
 
 func (p *Population) sortIndividualsByFitness() {
 	sort.Slice(p.Individuals, func(i, j int) bool {
-		fi := p.Individuals[i].Fitness(p.Target)
-		fj := p.Individuals[j].Fitness(p.Target)
+		fi := p.Individuals[i].CalculateFitness(p.Target)
+		fj := p.Individuals[j].CalculateFitness(p.Target)
 		return fi-fj < 0
 	})
+}
+
+func (p *Population) roulette() *Chromosome {
+	sum := 0.
+	lastFit := p.Individuals[len(p.Individuals)-1].Fitness
+	fits := []float64{}
+	for _, i := range p.Individuals {
+		sum += lastFit - i.Fitness
+		fits = append(fits, sum)
+	}
+	r := rand.Float64()
+	for i, fit := range fits {
+		if r < fit/sum {
+			return p.Individuals[i]
+		}
+	}
+	return p.Individuals[0]
 }
 
 func (p *Population) tournament(count int) *Chromosome {
 	c := p.Individuals[rand.Int63n(40)]
 	for i := 0; i < count-1; i++ {
 		c2 := p.Individuals[rand.Int63n(40)]
-		if c2.fitness < c.fitness {
+		if c2.Fitness < c.Fitness {
 			c = c2
 		}
 	}
@@ -130,15 +180,16 @@ func (p *Population) tournament(count int) *Chromosome {
 func (p *Population) PrintAverageFitness() {
 	sum := 0.
 	for _, in := range p.Individuals {
-		f := in.Fitness(p.Target)
+		f := in.CalculateFitness(p.Target)
 		sum += f
 	}
 	log.Printf("gen:%v - ave:%v\n", p.Generation, sum/float64(len(p.Individuals)))
 }
 
 type Chromosome struct {
-	Genes   []*Gene
-	fitness float64
+	Genes     []*Gene
+	Fitness   float64
+	Phenotype *image.RGBA
 }
 
 func NewChromosome(num int) *Chromosome {
@@ -152,16 +203,9 @@ func NewChromosome(num int) *Chromosome {
 
 func (c1 *Chromosome) Intersect(c2 *Chromosome) *Chromosome {
 	c := &Chromosome{}
-	ratio1 := c1.fitness / (c1.fitness + c2.fitness)
-	count1 := int(GeneCount * ratio1)
-	c.Genes = make([]*Gene, len(c1.Genes))
-	for i := 0; i < count1; i++ {
-		c.Genes[i] = c1.Genes[i]
-	}
-	for i := count1; i < len(c.Genes); i++ {
-		c.Genes[i] = c2.Genes[i]
-	}
+	ratio1 := c1.Fitness / (c1.Fitness + c2.Fitness)
 	/*
+		// ランダムに入れ替え
 		for i := 0; i < len(c.Genes); i++ {
 			if rand.Float64() < ratio1 {
 				c.Genes[i] = c1.Genes[i]
@@ -170,29 +214,66 @@ func (c1 *Chromosome) Intersect(c2 *Chromosome) *Chromosome {
 			}
 		}
 	*/
+
+	// 交差
+	count1 := int(GeneCount * ratio1)
+	c.Genes = make([]*Gene, len(c1.Genes))
+	for i := 0; i < count1; i++ {
+		c.Genes[i] = c1.Genes[i]
+	}
+	for i := count1; i < len(c.Genes); i++ {
+		c.Genes[i] = c2.Genes[i]
+	}
 	return c
+}
+
+func (c *Chromosome) Clone() *Chromosome {
+	clone := &Chromosome{}
+	clone.Genes = make([]*Gene, len(c.Genes))
+	for i, g := range c.Genes {
+		clone.Genes[i] = g.Clone()
+	}
+	return clone
+}
+
+func (c *Chromosome) Reset() {
+	c.Fitness = 0.
+	c.Phenotype = nil
 }
 
 func (c *Chromosome) Mutate(geneCount int) {
 	for i := 0; i < geneCount; i++ {
-		c.Genes[int(rand.Intn(len(c.Genes)))] = NewGene(time.Now().UnixNano())
+		r := rand.Intn(len(c.Genes))
+		if UseGeneMutate {
+			c.Genes[r].Mutate()
+		} else {
+			c.Genes[r] = NewGene(time.Now().UnixNano())
+		}
 	}
+	c.Reset()
 }
 
-func (c *Chromosome) Fitness(target image.Image) float64 {
-	if 0 < c.fitness {
-		return c.fitness
+func (c *Chromosome) Image() *image.RGBA {
+	if c.Phenotype != nil {
+		return c.Phenotype
+	}
+	return c.Decode()
+}
+
+func (c *Chromosome) CalculateFitness(target image.Image) float64 {
+	if 0 < c.Fitness {
+		return c.Fitness
 	}
 
-	result := c.Decode()
+	result := c.Image()
 	for y := 0; y < ImageSize; y++ {
 		for x := 0; x < ImageSize; x++ {
 			tr, tg, tb, _ := target.At(x, y).RGBA()
 			rr, rg, rb, _ := result.At(x, y).RGBA()
-			c.fitness += math.Abs(float64(tr-rr)) + math.Abs(float64(tg-rg)) + math.Abs(float64(tb-rb))
+			c.Fitness += math.Abs(float64(tr-rr)) + math.Abs(float64(tg-rg)) + math.Abs(float64(tb-rb))
 		}
 	}
-	return c.fitness
+	return c.Fitness
 }
 
 func (c *Chromosome) CheckGenes() {
@@ -204,10 +285,10 @@ func (c *Chromosome) CheckGenes() {
 }
 
 func (c *Chromosome) Decode() *image.RGBA {
-	img := image.NewRGBA(image.Rect(0, 0, ImageSize, ImageSize))
+	c.Phenotype = image.NewRGBA(image.Rect(0, 0, ImageSize, ImageSize))
 	for y := 0; y < ImageSize; y++ {
 		for x := 0; x < ImageSize; x++ {
-			img.Set(x, y, color.White)
+			c.Phenotype.Set(x, y, color.White)
 		}
 	}
 
@@ -218,9 +299,9 @@ func (c *Chromosome) Decode() *image.RGBA {
 	})
 	for _, gene := range c.Genes {
 		s := gene.Shape()
-		s.DrawOn(img)
+		s.DrawOn(c.Phenotype)
 	}
-	return img
+	return c.Phenotype
 }
 
 type Gene struct {
@@ -229,11 +310,26 @@ type Gene struct {
 
 func NewGene(seed int64) *Gene {
 	gene := &Gene{}
-	r := rand.New(rand.NewSource(seed))
 	for i, _ := range gene.Properties {
-		gene.Properties[i] = r.Float64()
+		gene.Properties[i] = rand.Float64()
 	}
 	return gene
+}
+
+func (g *Gene) Clone() *Gene {
+	clone := &Gene{}
+	clone.Properties = [LocusCount]float64{}
+	for i, p := range g.Properties {
+		clone.Properties[i] = p
+	}
+	return clone
+}
+
+func (g *Gene) Mutate() {
+	for i := 0; i < 1; i++ {
+		n := rand.Intn(len(g.Properties))
+		g.Properties[n] = rand.Float64()
+	}
 }
 
 func (g *Gene) Check() bool {
@@ -283,7 +379,7 @@ func newShapeCommon(props [10]float64) shapeCommon {
 }
 
 func (s *shapeCommon) _blend(base uint32, added, alpha uint8) uint8 {
-	a := 0.5 + float64(alpha)/511 // 0.5 <= alpha < 1.0
+	a := float64(alpha)
 	return uint8(float64(base)*(1-a) + float64(added)*a)
 
 }
@@ -291,12 +387,24 @@ func (s *shapeCommon) _blend(base uint32, added, alpha uint8) uint8 {
 func (s *shapeCommon) blend(baseImage *image.RGBA, x, y int) {
 	c := s.Color
 	r, g, b, _ := baseImage.At(x, y).RGBA()
-	baseImage.Set(x, y, color.RGBA{
-		s._blend(r, c.R, c.A),
-		s._blend(g, c.G, c.A),
-		s._blend(b, c.B, c.A),
-		255,
-	})
+
+	if UseAlpha {
+		// アルファを考慮
+		baseImage.Set(x, y, color.RGBA{
+			s._blend(r, c.R, c.A),
+			s._blend(g, c.G, c.A),
+			s._blend(b, c.B, c.A),
+			255,
+		})
+	} else {
+		// アルファを無視
+		baseImage.Set(x, y, color.RGBA{
+			s.Color.R,
+			s.Color.G,
+			s.Color.B,
+			255,
+		})
+	}
 }
 
 func (s *shapeCommon) drawOn(img *image.RGBA, area func(cx, cy, w, h, x, y, ar, r float64) bool) {
@@ -309,9 +417,15 @@ func (s *shapeCommon) drawOn(img *image.RGBA, area func(cx, cy, w, h, x, y, ar, 
 
 	for dy := -h / 2; dy < h/2; dy++ {
 		y := cy + dy
+		if y < 0 || ImageSize < y {
+			continue
+		}
 		yi := int(y)
 		for dx := -w / 2; dx < w/2; dx++ {
 			x := cx + dx
+			if x < 0 || ImageSize < x {
+				continue
+			}
 			if area(cx, cy, w, h, x, y, ar, r) {
 				s.blend(img, int(x), yi)
 			}
@@ -331,7 +445,7 @@ func NewRectangle(props [10]float64) *Rectangle {
 
 func (r *Rectangle) DrawOn(img *image.RGBA) {
 	r.drawOn(img, func(cx, cy, w, h, x, y, _, _ float64) bool {
-		return cx-w/2 < x && x < cx+w/2 && cy-h/2 < y && y < cy+h/2
+		return true
 	})
 }
 
